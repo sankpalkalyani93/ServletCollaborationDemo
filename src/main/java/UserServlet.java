c53a7c6ad7d9b605d464bc5c473cc4652742847b
kalyani <sankpalkalyani93@gmail.com>
2024-06-21 12:33:16 +0530

    first commit

---
+++ src/main/java/UserServlet.java
@@ -0,0 +1,49 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+@WebServlet("/UserServlet")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public UserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		response.setContentType("text/html");
+		
+		String userName = request.getParameter("userName");
+		String password = request.getParameter("password");
+		
+		RequestDispatcher rd;
+		if(userName != null) {
+			request.setAttribute("userName", userName);
+			rd = request.getRequestDispatcher("profile.jsp");
+			rd.forward(request, response);
+		}
+	
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
